603ae0945fe8956689f26a594ce504f1b1f011ba
lawlowlight <marhebasd@naver.com>
2022-05-02 11:49:39 +0900

    220502 파이썬 기본 문법학습

---
+++ basic/datatype01.py
@@ -0,0 +1,24 @@
+# 데이터타입
+
+a = 0b101011
+f = 0b11111111
+print(a)
+print(f)
+print(bin(10))
+
+b = print(0o52)
+print(oct(10))
+
+print(0xff)
+
+salary = 4_500_000 # 통화단위
+print(salary)
+
+print(type(1))
+print(type(3.14))
+z = False
+print(type(z))
+
+z = 10
+print(type(z))
+
---
+++ basic/datetype02.py
@@ -0,0 +1,13 @@
+# 문자열
+from audioop import mul
+
+
+bruce_ecke1= 'Life is short.\n\tYou need Python.'
+print(bruce_ecke1)
+
+# 여러줄 문자열
+multi_line = '''Life is short.
+You need Python.
+And i need C#, too.
+'''
+print(multi_line)
